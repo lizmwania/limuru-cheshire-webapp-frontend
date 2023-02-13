12c95829442fa10dc0ea3f5c971da5c0350cdb2c
elvmariga <elvmariga@gmail.com>
2023-02-13 12:49:57 +0300

    responsiveness at contact section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,20 +2,15 @@ import React from 'react';
 
 const Contact = () => {
     return (
-        <div className='contact_container'>
-            <div className='title_wrapper'>
-                <p>Have a question?</p>
-                <h2>
-                    Get in touch with us.
-                </h2>
-            </div>
-            <div className='btn_group'>
-                <div className='btn btn_primary'>
-                    Contact Us
-                </div>
-            </div>
-            
+      <div className="contact_container">
+        <div className="title_wrapper">
+          <p>Have a question?</p>
+          <h2>Get in touch with us.</h2>
+          <div className="btn_group">
+            <div className="btn btn_primary">Contact Us</div>
+          </div>
         </div>
+      </div>
     );
 }
 
